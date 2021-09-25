69a84d4894a65b3caf0094372715abaa850d961a
Keerthana <keerthana@blugraph.com>
2021-09-25 09:09:36 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-
+const path = require('path');
 
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -187,6 +187,11 @@ app.post('/admin', (req, res) => {
 })
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(5000, () => {
     console.log("server running")
